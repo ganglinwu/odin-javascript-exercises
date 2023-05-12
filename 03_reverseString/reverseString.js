c41855c7cebf8400fd55f61a7d3ff7ca414417cb
ganglinwu <ganglin.wu@gmail.com>
2023-05-12 16:02:39 +0800

    Completed exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reverseArray = [];
+    for (let i = 1; i<=str.length; i++) {
+        reverseArray.push(str.at(-1*i));
+    }
+    return reverseArray.join('')
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
