e3142f29127250b7b8e2e2955084d87c88c85661
ganglinwu <ganglin.wu@gmail.com>
2023-05-12 19:28:36 +0800

    Completed exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(intStart, intEnd) {
+    // just in case intStart is greater than intEnd
+    if (intStart>intEnd) {
+        let temp=intStart;
+        intStart=intEnd;
+        intEnd=temp;
+    }
 
+    // check for negative numbers
+    if (typeof(intStart) != "number" || typeof(intEnd)!= 'number') {
+        return 'ERROR';
+    }
+    
+    // check for non number type
+    if (intStart <0 || intEnd<0) {
+        return 'ERROR';
+    }
+    
+
+
+    let sum = 0;
+    for (let i = intStart; i<=intEnd; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+sumAll(10, 1);
 
 // Do not edit below this line
 module.exports = sumAll;
