1c2fd00ece0c58329c714de544a3ef633ca2b612
ganglinwu <ganglin.wu@gmail.com>
2023-05-12 19:14:15 +0800

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...intToRemoveArray) {
+    for (let i =0; i<intToRemoveArray.length; i++) {
+        if (arr.includes(intToRemoveArray[i])) {
+            indexToBeRemoved = arr.indexOf(intToRemoveArray[i]);
+            arr.splice(indexToBeRemoved,1);
+        }
+        else {
+            continue
+        }
+    }
+    return arr
 };
+
+removeFromArray([1,2,3,4], 3);
 
 // Do not edit below this line
 module.exports = removeFromArray;
