07e06daec6cbb601561c15323482e9799f61b259
ganglinwu <ganglin.wu@gmail.com>
2023-05-12 15:49:31 +0800

    Completed exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+function repeatString(str, int){
+    if (int >= 0) {
+        return str.repeat(int);
+    }
+    else {
+        return 'ERROR'
+    }
 };
+
+repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
