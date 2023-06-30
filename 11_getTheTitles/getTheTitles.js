f6dd2b0b01f91dc2360173d56d531564a61d4f5f
ganglinwu <ganglin.wu@gmail.com>
2023-06-30 20:48:42 +0800

    Completed exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    arr = books.map((book)=> {
+        return book.title;
+    })
+    return arr;
 };
 
 // Do not edit below this line
