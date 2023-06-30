37f6f207fafdbddfef93c5b50e6f98ee46df0510
ganglinwu <ganglin.wu@gmail.com>
2023-06-30 17:38:21 +0800

    Implemented basic palindrome checker.
    however unable to ignore punctuation and spaces

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const array = str.toLowerCase().split('');
+    const reverseArray = array.reduce((arr,letter)=>{
+        if (letter.toString().charCodeAt(0)>= 97 && letter.toString().charCodeAt(0)<= 122) {
+            arr.unshift(letter);
+        }
+        return arr;
+    },[])
+    reverseStr = reverseArray.join('');
+    return (str === reverseStr) ? true : false;
 };
+
+console.log('1 ' + palindromes('racecar'));
+console.log('2 ' + palindromes('racecar!'));
+console.log('3 ' + palindromes('Racecar!'));
+
+// need to think of a way to exclude punctuation
 
 // Do not edit below this line
 module.exports = palindromes;
