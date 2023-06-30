a20b49ac251ac20c343612ba45c695a5431c58f2
ganglinwu <ganglin.wu@gmail.com>
2023-06-30 21:01:16 +0800

    Completed exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+var todayDate = new Date;
+var currentYear = todayDate.getFullYear()
 
+const findTheOldest = function(people) {
+    const sortedByOldest = people.sort((person1, person2)=> {
+        if (!person1.yearOfDeath) {
+            person1.yearOfDeath = currentYear;
+        }
+        if (!person2.yearOfDeath) {
+            person2.yearOfDeath = currentYear;
+        }
+        age1 = person1.yearOfDeath - person1.yearOfBirth;
+        age2 = person2.yearOfDeath - person2.yearOfBirth;
+        return age1 > age2 ? -1 : 1;
+    })
+    return sortedByOldest[0];
 };
 
 // Do not edit below this line
